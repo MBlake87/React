bab1d319a486d96dd02daf5a27f9890a105382eb
MBlake87 <markblake1987@gmail.com>
2024-01-22 12:17:43 +0000

    mobile update

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -7,11 +7,11 @@ export function Home() {
         id="home"
         className="flex w-[100vw] flex-col items-center justify-center xl:h-[75vh] xl:flex-row"
       >
-        <h1 className=" text-[5rem] leading-[1]  text-text xl:text-[15rem]">
+        <h1 className=" mt-[5rem] text-[5rem] leading-[1]  text-text xl:text-[15rem]">
           Mark Blake
         </h1>
       </div>
-      <p className="py-10 text-center text-[1rem] tracking-[1rem] text-text xl:p-10 xl:text-[2rem] xl:tracking-[2rem]">
+      <p className="mb-10 py-10 text-center text-[1rem] tracking-[1rem] text-text xl:p-10 xl:text-[2rem] xl:tracking-[2rem]">
         Front End Developer
       </p>
     </>
